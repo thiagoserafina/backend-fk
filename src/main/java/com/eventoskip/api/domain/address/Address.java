7f19d56e848b03301dd98da3c945dd0d6556bb3b
Thiago Serafina <thiagoserafina@unesc.net>
2024-06-14 15:40:16 -0300

    feat: creating entities

---
+++ src/main/java/com/eventoskip/api/domain/address/Address.java
@@ -0,0 +1,29 @@
+package com.eventoskip.api.domain.address;
+
+import com.eventoskip.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
