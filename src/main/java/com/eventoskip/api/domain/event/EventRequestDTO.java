fe07120e3d8a8da383d5238ae27b8f9c9a2be152
Thiago Serafina <thiagoserafina@unesc.net>
2024-06-14 18:09:14 -0300

    feat: add AWSConfig; Scripts SQL; EventRequestDTO; EventService

---
+++ src/main/java/com/eventoskip/api/domain/event/EventRequestDTO.java
@@ -0,0 +1,8 @@
+package com.eventoskip.api.domain.event;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
+public record EventRequestDTO(String title, String description, Long date, String city, String uf, Boolean remote, String eventUrl, MultipartFile image) {
+}
